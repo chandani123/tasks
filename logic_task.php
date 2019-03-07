bb8a15ead1e3327bab04e0c31eaab46505fd4901
chandani123 <40708857+chandani123@users.noreply.github.com>
2019-03-07 08:00:26 +0530

    Add files via upload

--- logic_task.php
+++ logic_task.php
@@ -8,8 +8,24 @@ $con = $connection->database();
 
 for($i=1;$i<=100;$i++)
 {
-   echo $i;
+    if (($i % 3 == 0) && ($i % 5 == 0))
+    {
+        $re = "foobar";
+    }
+    else if($i % 3 == 0)
+    {
+        $re = "foo";
+    }
+    else if($i % 5 == 0)
+    {
+        $re = "bar";
+    }
 
+    else
+    {
+        $re = $i;
+    }
+    echo $re.", ";
 }
 
 ?>
