97e107dc4a3432d2a19226fa7a22059f9bad1ef5
chandani123 <40708857+chandani123@users.noreply.github.com>
2019-03-07 07:44:46 +0530

    Add files via upload

--- user_upload.php
+++ user_upload.php
@@ -1,24 +1,21 @@
 <?php
+/*
+    => Get a file path for passing cmd
+    => for ex: php user_upload.php C:\Users\Dhaval\Desktop\Task\users.csv
+    => ucwords used to first letter capital from string
+    => strtolower used to other letter small from string
+*/
+
 ini_set('display_errors', 0);
 error_reporting(0);
+include("Db_connection.php");
+$connection = new database();
+$con = $connection->database();
 
-    // Db Connection
-    $con = mysqli_connect("localhost","root","","catalyst_task");
-
-    /* Get a file path for passing cmd
-    for ex: php user_upload.php C:\Users\Dhaval\Desktop\Task\users.csv */
-
-    $filename = $argv[1];
-    {
+        $filename = $argv[1];
         $file = fopen($filename, "r");
         while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
         {
-
-            /* ucwords used to first letter capital from string
-               strtolower used to other letter small from string
-            */
-
-
             if (!preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", strtolower($getData[2]))){
                 echo "Invalid Email Address is : ".strtolower($getData[2]);
 
@@ -40,8 +37,7 @@ error_reporting(0);
 
         }
         else {
-            echo "Success";
+            echo "FILE IMPORT SUCCESSFULLY";
         }
         fclose($file);
-    }
 ?>
