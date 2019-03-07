4f9b422c58fb5f1f5e53a0ee8aa3bef5443cd3af
chandani123 <40708857+chandani123@users.noreply.github.com>
2019-03-07 07:44:26 +0530

    Add files via upload

---
+++ Db_connection.php
@@ -0,0 +1,45 @@
+<?php
+class database {
+
+    var $_sql			= '';
+    /** @var Internal variable to hold the connector resource */
+    var $_resource		= '';
+    /** @var Internal variable to hold the query result*/
+    var $_result        = '';
+    /** @var Internal variable to hold the query result*/
+    var $_insert_id        = '';
+
+    //$host = '';
+    /**
+     * Database object constructor
+     * @param string Database host
+     * @param string Database user name
+     * @param string Database user password
+     * @param string Database name
+     * @param string Common prefix for all tables
+     * @param boolean If true and there is an error, go offline
+     */
+    function database() {
+
+        $con = mysqli_connect("localhost","root","","catalyst_task");
+        return $con;
+    }
+
+    /**
+     * Execute the query
+     * @return mixed A database resource if successful, FALSE if not.
+     */
+
+
+
+    function dbClose(){
+        mysql_close($this->_resource);
+    }
+
+    function fetchArray($rs){
+
+        return @mysql_fetch_array($rs);
+    }
+
+}
+?>
